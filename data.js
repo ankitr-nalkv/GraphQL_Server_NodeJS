0d394903f5983bbbc26f180f0e0466a4ae274443
Ankit Rathore <Ankit.Rathore@ivanti.com>
2021-12-01 16:57:15 +0530

    Adding sample data

--- data.js
+++ data.js
@@ -26,6 +26,384 @@ var incidentsData = [
         keywords: 'JavaScript',
         link: 'https://codingthesmartway.com/incidents/understand-javascript/',
     },
+    {
+        id: 1,
+        subject: 'The Complete Node.js Developer incident',
+        owner: 'Andrew Mead, Rob Percival',
+        description:
+            'Learn Node.js by building real-world applications with Node, Express, MongoDB, Mocha, and more!',
+        keywords: 'Node.js',
+        link: 'https://codingthesmartway.com/incidents/nodejs/',
+    },
+    {
+        id: 2,
+        subject: 'Node.js, Express & MongoDB Dev to Deployment',
+        owner: 'Brad Traversy',
+        description:
+            'Learn by example building & deploying real-world Node.js applications from absolute scratch',
+        keywords: 'Node.js',
+        link: 'https://codingthesmartway.com/incidents/nodejs-express-mongodb/',
+    },
+    {
+        id: 3,
+        subject: 'JavaScript: Understanding The Weird Parts',
+        owner: 'Anthony Alicea',
+        description:
+            'An advanced JavaScript incident for everyone! Scope, closures, prototypes, this, build your own framework, and more.',
+        keywords: 'JavaScript',
+        link: 'https://codingthesmartway.com/incidents/understand-javascript/',
+    },
+    {
+        id: 1,
+        subject: 'The Complete Node.js Developer incident',
+        owner: 'Andrew Mead, Rob Percival',
+        description:
+            'Learn Node.js by building real-world applications with Node, Express, MongoDB, Mocha, and more!',
+        keywords: 'Node.js',
+        link: 'https://codingthesmartway.com/incidents/nodejs/',
+    },
+    {
+        id: 2,
+        subject: 'Node.js, Express & MongoDB Dev to Deployment',
+        owner: 'Brad Traversy',
+        description:
+            'Learn by example building & deploying real-world Node.js applications from absolute scratch',
+        keywords: 'Node.js',
+        link: 'https://codingthesmartway.com/incidents/nodejs-express-mongodb/',
+    },
+    {
+        id: 3,
+        subject: 'JavaScript: Understanding The Weird Parts',
+        owner: 'Anthony Alicea',
+        description:
+            'An advanced JavaScript incident for everyone! Scope, closures, prototypes, this, build your own framework, and more.',
+        keywords: 'JavaScript',
+        link: 'https://codingthesmartway.com/incidents/understand-javascript/',
+    },
+    {
+        id: 1,
+        subject: 'The Complete Node.js Developer incident',
+        owner: 'Andrew Mead, Rob Percival',
+        description:
+            'Learn Node.js by building real-world applications with Node, Express, MongoDB, Mocha, and more!',
+        keywords: 'Node.js',
+        link: 'https://codingthesmartway.com/incidents/nodejs/',
+    },
+    {
+        id: 2,
+        subject: 'Node.js, Express & MongoDB Dev to Deployment',
+        owner: 'Brad Traversy',
+        description:
+            'Learn by example building & deploying real-world Node.js applications from absolute scratch',
+        keywords: 'Node.js',
+        link: 'https://codingthesmartway.com/incidents/nodejs-express-mongodb/',
+    },
+    {
+        id: 3,
+        subject: 'JavaScript: Understanding The Weird Parts',
+        owner: 'Anthony Alicea',
+        description:
+            'An advanced JavaScript incident for everyone! Scope, closures, prototypes, this, build your own framework, and more.',
+        keywords: 'JavaScript',
+        link: 'https://codingthesmartway.com/incidents/understand-javascript/',
+    },
+    {
+        id: 1,
+        subject: 'The Complete Node.js Developer incident',
+        owner: 'Andrew Mead, Rob Percival',
+        description:
+            'Learn Node.js by building real-world applications with Node, Express, MongoDB, Mocha, and more!',
+        keywords: 'Node.js',
+        link: 'https://codingthesmartway.com/incidents/nodejs/',
+    },
+    {
+        id: 2,
+        subject: 'Node.js, Express & MongoDB Dev to Deployment',
+        owner: 'Brad Traversy',
+        description:
+            'Learn by example building & deploying real-world Node.js applications from absolute scratch',
+        keywords: 'Node.js',
+        link: 'https://codingthesmartway.com/incidents/nodejs-express-mongodb/',
+    },
+    {
+        id: 3,
+        subject: 'JavaScript: Understanding The Weird Parts',
+        owner: 'Anthony Alicea',
+        description:
+            'An advanced JavaScript incident for everyone! Scope, closures, prototypes, this, build your own framework, and more.',
+        keywords: 'JavaScript',
+        link: 'https://codingthesmartway.com/incidents/understand-javascript/',
+    },
+    {
+        id: 1,
+        subject: 'The Complete Node.js Developer incident',
+        owner: 'Andrew Mead, Rob Percival',
+        description:
+            'Learn Node.js by building real-world applications with Node, Express, MongoDB, Mocha, and more!',
+        keywords: 'Node.js',
+        link: 'https://codingthesmartway.com/incidents/nodejs/',
+    },
+    {
+        id: 2,
+        subject: 'Node.js, Express & MongoDB Dev to Deployment',
+        owner: 'Brad Traversy',
+        description:
+            'Learn by example building & deploying real-world Node.js applications from absolute scratch',
+        keywords: 'Node.js',
+        link: 'https://codingthesmartway.com/incidents/nodejs-express-mongodb/',
+    },
+    {
+        id: 3,
+        subject: 'JavaScript: Understanding The Weird Parts',
+        owner: 'Anthony Alicea',
+        description:
+            'An advanced JavaScript incident for everyone! Scope, closures, prototypes, this, build your own framework, and more.',
+        keywords: 'JavaScript',
+        link: 'https://codingthesmartway.com/incidents/understand-javascript/',
+    },
+    {
+        id: 1,
+        subject: 'The Complete Node.js Developer incident',
+        owner: 'Andrew Mead, Rob Percival',
+        description:
+            'Learn Node.js by building real-world applications with Node, Express, MongoDB, Mocha, and more!',
+        keywords: 'Node.js',
+        link: 'https://codingthesmartway.com/incidents/nodejs/',
+    },
+    {
+        id: 2,
+        subject: 'Node.js, Express & MongoDB Dev to Deployment',
+        owner: 'Brad Traversy',
+        description:
+            'Learn by example building & deploying real-world Node.js applications from absolute scratch',
+        keywords: 'Node.js',
+        link: 'https://codingthesmartway.com/incidents/nodejs-express-mongodb/',
+    },
+    {
+        id: 3,
+        subject: 'JavaScript: Understanding The Weird Parts',
+        owner: 'Anthony Alicea',
+        description:
+            'An advanced JavaScript incident for everyone! Scope, closures, prototypes, this, build your own framework, and more.',
+        keywords: 'JavaScript',
+        link: 'https://codingthesmartway.com/incidents/understand-javascript/',
+    },
+    {
+        id: 1,
+        subject: 'The Complete Node.js Developer incident',
+        owner: 'Andrew Mead, Rob Percival',
+        description:
+            'Learn Node.js by building real-world applications with Node, Express, MongoDB, Mocha, and more!',
+        keywords: 'Node.js',
+        link: 'https://codingthesmartway.com/incidents/nodejs/',
+    },
+    {
+        id: 2,
+        subject: 'Node.js, Express & MongoDB Dev to Deployment',
+        owner: 'Brad Traversy',
+        description:
+            'Learn by example building & deploying real-world Node.js applications from absolute scratch',
+        keywords: 'Node.js',
+        link: 'https://codingthesmartway.com/incidents/nodejs-express-mongodb/',
+    },
+    {
+        id: 3,
+        subject: 'JavaScript: Understanding The Weird Parts',
+        owner: 'Anthony Alicea',
+        description:
+            'An advanced JavaScript incident for everyone! Scope, closures, prototypes, this, build your own framework, and more.',
+        keywords: 'JavaScript',
+        link: 'https://codingthesmartway.com/incidents/understand-javascript/',
+    },
+    {
+        id: 1,
+        subject: 'The Complete Node.js Developer incident',
+        owner: 'Andrew Mead, Rob Percival',
+        description:
+            'Learn Node.js by building real-world applications with Node, Express, MongoDB, Mocha, and more!',
+        keywords: 'Node.js',
+        link: 'https://codingthesmartway.com/incidents/nodejs/',
+    },
+    {
+        id: 2,
+        subject: 'Node.js, Express & MongoDB Dev to Deployment',
+        owner: 'Brad Traversy',
+        description:
+            'Learn by example building & deploying real-world Node.js applications from absolute scratch',
+        keywords: 'Node.js',
+        link: 'https://codingthesmartway.com/incidents/nodejs-express-mongodb/',
+    },
+    {
+        id: 3,
+        subject: 'JavaScript: Understanding The Weird Parts',
+        owner: 'Anthony Alicea',
+        description:
+            'An advanced JavaScript incident for everyone! Scope, closures, prototypes, this, build your own framework, and more.',
+        keywords: 'JavaScript',
+        link: 'https://codingthesmartway.com/incidents/understand-javascript/',
+    },
+    {
+        id: 1,
+        subject: 'The Complete Node.js Developer incident',
+        owner: 'Andrew Mead, Rob Percival',
+        description:
+            'Learn Node.js by building real-world applications with Node, Express, MongoDB, Mocha, and more!',
+        keywords: 'Node.js',
+        link: 'https://codingthesmartway.com/incidents/nodejs/',
+    },
+    {
+        id: 2,
+        subject: 'Node.js, Express & MongoDB Dev to Deployment',
+        owner: 'Brad Traversy',
+        description:
+            'Learn by example building & deploying real-world Node.js applications from absolute scratch',
+        keywords: 'Node.js',
+        link: 'https://codingthesmartway.com/incidents/nodejs-express-mongodb/',
+    },
+    {
+        id: 3,
+        subject: 'JavaScript: Understanding The Weird Parts',
+        owner: 'Anthony Alicea',
+        description:
+            'An advanced JavaScript incident for everyone! Scope, closures, prototypes, this, build your own framework, and more.',
+        keywords: 'JavaScript',
+        link: 'https://codingthesmartway.com/incidents/understand-javascript/',
+    },
+    {
+        id: 1,
+        subject: 'The Complete Node.js Developer incident',
+        owner: 'Andrew Mead, Rob Percival',
+        description:
+            'Learn Node.js by building real-world applications with Node, Express, MongoDB, Mocha, and more!',
+        keywords: 'Node.js',
+        link: 'https://codingthesmartway.com/incidents/nodejs/',
+    },
+    {
+        id: 2,
+        subject: 'Node.js, Express & MongoDB Dev to Deployment',
+        owner: 'Brad Traversy',
+        description:
+            'Learn by example building & deploying real-world Node.js applications from absolute scratch',
+        keywords: 'Node.js',
+        link: 'https://codingthesmartway.com/incidents/nodejs-express-mongodb/',
+    },
+    {
+        id: 3,
+        subject: 'JavaScript: Understanding The Weird Parts',
+        owner: 'Anthony Alicea',
+        description:
+            'An advanced JavaScript incident for everyone! Scope, closures, prototypes, this, build your own framework, and more.',
+        keywords: 'JavaScript',
+        link: 'https://codingthesmartway.com/incidents/understand-javascript/',
+    },
+    {
+        id: 1,
+        subject: 'The Complete Node.js Developer incident',
+        owner: 'Andrew Mead, Rob Percival',
+        description:
+            'Learn Node.js by building real-world applications with Node, Express, MongoDB, Mocha, and more!',
+        keywords: 'Node.js',
+        link: 'https://codingthesmartway.com/incidents/nodejs/',
+    },
+    {
+        id: 2,
+        subject: 'Node.js, Express & MongoDB Dev to Deployment',
+        owner: 'Brad Traversy',
+        description:
+            'Learn by example building & deploying real-world Node.js applications from absolute scratch',
+        keywords: 'Node.js',
+        link: 'https://codingthesmartway.com/incidents/nodejs-express-mongodb/',
+    },
+    {
+        id: 3,
+        subject: 'JavaScript: Understanding The Weird Parts',
+        owner: 'Anthony Alicea',
+        description:
+            'An advanced JavaScript incident for everyone! Scope, closures, prototypes, this, build your own framework, and more.',
+        keywords: 'JavaScript',
+        link: 'https://codingthesmartway.com/incidents/understand-javascript/',
+    },
+    {
+        id: 1,
+        subject: 'The Complete Node.js Developer incident',
+        owner: 'Andrew Mead, Rob Percival',
+        description:
+            'Learn Node.js by building real-world applications with Node, Express, MongoDB, Mocha, and more!',
+        keywords: 'Node.js',
+        link: 'https://codingthesmartway.com/incidents/nodejs/',
+    },
+    {
+        id: 2,
+        subject: 'Node.js, Express & MongoDB Dev to Deployment',
+        owner: 'Brad Traversy',
+        description:
+            'Learn by example building & deploying real-world Node.js applications from absolute scratch',
+        keywords: 'Node.js',
+        link: 'https://codingthesmartway.com/incidents/nodejs-express-mongodb/',
+    },
+    {
+        id: 3,
+        subject: 'JavaScript: Understanding The Weird Parts',
+        owner: 'Anthony Alicea',
+        description:
+            'An advanced JavaScript incident for everyone! Scope, closures, prototypes, this, build your own framework, and more.',
+        keywords: 'JavaScript',
+        link: 'https://codingthesmartway.com/incidents/understand-javascript/',
+    },
+    {
+        id: 1,
+        subject: 'The Complete Node.js Developer incident',
+        owner: 'Andrew Mead, Rob Percival',
+        description:
+            'Learn Node.js by building real-world applications with Node, Express, MongoDB, Mocha, and more!',
+        keywords: 'Node.js',
+        link: 'https://codingthesmartway.com/incidents/nodejs/',
+    },
+    {
+        id: 2,
+        subject: 'Node.js, Express & MongoDB Dev to Deployment',
+        owner: 'Brad Traversy',
+        description:
+            'Learn by example building & deploying real-world Node.js applications from absolute scratch',
+        keywords: 'Node.js',
+        link: 'https://codingthesmartway.com/incidents/nodejs-express-mongodb/',
+    },
+    {
+        id: 3,
+        subject: 'JavaScript: Understanding The Weird Parts',
+        owner: 'Anthony Alicea',
+        description:
+            'An advanced JavaScript incident for everyone! Scope, closures, prototypes, this, build your own framework, and more.',
+        keywords: 'JavaScript',
+        link: 'https://codingthesmartway.com/incidents/understand-javascript/',
+    },
+    {
+        id: 1,
+        subject: 'The Complete Node.js Developer incident',
+        owner: 'Andrew Mead, Rob Percival',
+        description:
+            'Learn Node.js by building real-world applications with Node, Express, MongoDB, Mocha, and more!',
+        keywords: 'Node.js',
+        link: 'https://codingthesmartway.com/incidents/nodejs/',
+    },
+    {
+        id: 2,
+        subject: 'Node.js, Express & MongoDB Dev to Deployment',
+        owner: 'Brad Traversy',
+        description:
+            'Learn by example building & deploying real-world Node.js applications from absolute scratch',
+        keywords: 'Node.js',
+        link: 'https://codingthesmartway.com/incidents/nodejs-express-mongodb/',
+    },
+    {
+        id: 3,
+        subject: 'JavaScript: Understanding The Weird Parts',
+        owner: 'Anthony Alicea',
+        description:
+            'An advanced JavaScript incident for everyone! Scope, closures, prototypes, this, build your own framework, and more.',
+        keywords: 'JavaScript',
+        link: 'https://codingthesmartway.com/incidents/understand-javascript/',
+    },
 ];
 
 module.exports = { incidentsData };
